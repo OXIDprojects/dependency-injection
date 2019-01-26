d6af542cd1c84481c24b46ead920dcb926f38537
Tobias Matthaiou <developer@tobimat.eu>
2019-01-26 18:42:10 +0100

    Base code

---
+++ function_project_container.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tobi
+ * Date: 2019-01-26
+ * Time: 18:38
+ */
---
+++ src/Container.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tobi
+ * Date: 2019-01-26
+ * Time: 18:40
+ */
+
+namespace oxidprojects\DI;
+
+/**
+ * Class Container
+ * @package oxidprojects\DI
+ */
+class Container
+{
+
+}
